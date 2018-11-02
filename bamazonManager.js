c34afb8d46745003e2482b1ba1351b64c563cf55
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-02 18:09:32 -0400

    one and two done

--- bamazonManager.js
+++ bamazonManager.js
@@ -39,7 +39,7 @@ function managerMenu() {
             ]
         })
         .then(function (response) {
-            switch(response.selection) {
+            switch (response.selection) {
                 case 'View Products for Sale':
                     viewInventory();
                     break;
@@ -47,7 +47,7 @@ function managerMenu() {
                     lowInventory();
                     break;
                 case 'Add to Inventory':
-                    restockInventory();
+                    restockInfo();
                     break;
                 case 'Add New Product':
                     newProduct();
@@ -60,7 +60,7 @@ function managerMenu() {
 }
 
 function viewInventory() {
-    connection.query(queryAll, function(error, res) {
+    connection.query(queryAll, function (error, res) {
         if (error) throw error;
         console.table(res);
         managerMenu();
@@ -68,13 +68,99 @@ function viewInventory() {
 }
 
 function lowInventory() {
-    connection.query(`SELECT * FROM products WHERE stock_quantity < 100`, function(error, res) {
+    connection.query(`SELECT * FROM products WHERE stock_quantity < 100`, function (error, res) {
         if (error) throw error;
-        console.table(res);
-        managerMenu();
+        if (res.length > 0) {
+            console.table(res);
+            managerMenu();
+        } else {
+            console.log('There are no low inventory items.'.green)
+            managerMenu();
+        }
     });
 }
 
+function restockInfo() {
+    idArray = [];
+    //let updateID;
+    //let updateQuantity;
+    connection.query("SELECT item_id FROM products", function (error, res) {
+        if (error) throw error;
+        res.forEach(item => {
+            idArray.push(item.item_id);
+        });
+    });
+    inquirer
+        .prompt([
+            {
+                name: 'id',
+                type: 'input',
+                message: 'What is the ID number of the product you wish to restock?',
+                validate: function (value) {
+                    return isNaN(value) === false;
+                }
+            },
+            {
+                name: 'restock_quantity',
+                type: 'input',
+                message: 'How many should be added to the inventory?',
+                validate: function (value) {
+                    return isNaN(value) === false;
+                }
+            }
+        ])
+        .then(function (update) {
+            let updateID = parseInt(update.id);
+            if (idArray.includes(updateID) === true) {
+                connection.query("SELECT stock_quantity FROM products WHERE ?", {item_id: update.id}, function (err, res) {
+                    //console.log(res[0].stock_quantity);
+                    let updateQuantity = res[0].stock_quantity + parseInt(update.restock_quantity);
+                    //console.log(updateID, updateQuantity);
+                    restockInventory(updateID, updateQuantity);
+                });
+            } else {
+                console.log(`!! ${update.id} is not a valid ID Number currently in the Stock System !!`.red);
+                restockInfo();
+            }
+        });
+}
+
+function restockInventory(id, quantity) {
+    connection.query(`${queryUpdate} ${queryWhere}`, [{stock_quantity: quantity}, {item_id: id}], function (err, res) {
+        console.log(`${res.affectedRows} successfully updated.`);
+        restockMenu();
+    });
+}
+
+function restockMenu() {
+    inquirer
+        .prompt({
+            name: 'selection',
+            type: 'list',
+            message: 'What would you like to do?'.yellow,
+            choices: [
+                'Main Menu',
+                'Restock More Items',
+                'Exit'
+            ]
+        })
+        .then(function (response) {
+            switch (response.selection) {
+                case 'Main Menu':
+                    managerMenu();
+                    break;
+                case 'Restock More Items':
+                    restockInfo();
+                    break;
+                case 'Exit':
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+
+/*
 function restockInventory() {
     idArray = [];
     connection.query("SELECT item_id FROM products", function(error, res) {
@@ -105,10 +191,18 @@ function restockInventory() {
         .then(function(update) {
             console.log(update);
             if (idArray.includes(parseInt(update.id)) === true) {
-                console.log('you can update this')
+                connection.query(`${queryUpdate} ${queryWhere}`,
+                    [
+                        {
+                            stock_quantity: (stock_quantity += update.restock_quantity)
+                        },
+                        {
+                            item_id: update.id
+                        }
+                    ])
             } else {
                 console.log(`${update.id} is not a valid ID Number currently in the Stock System`);
                 managerMenu();
             }
         })
-}
+}*/
