758d80fad098d414efb1966540d6f071a9c4f7ec
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-02 16:45:04 -0400

    finishing up part 2

--- bamazonManager.js
+++ bamazonManager.js
@@ -19,6 +19,61 @@ const connection = mysql.createConnection({
 
 connection.connect(function (error) {
     if (error) throw error;
-    console.log(`Connected as ID: ${connection.threadId}`);
-    connection.end();
+    //console.log(`Connected as ID: ${connection.threadId}`);
+    managerMenu();
 });
+
+function managerMenu() {
+    inquirer
+        .prompt({
+            name: 'selection',
+            type: 'list',
+            message: 'What would you like to do?'.yellow,
+            choices: [
+                'View Products for Sale',
+                'View Low Inventory',
+                'Add to Inventory',
+                'Add New Product',
+                'Exit'
+            ]
+        })
+        .then(function (response) {
+            switch(response.selection) {
+                case 'View Products for Sale':
+                    viewInventory();
+                    break;
+                case 'View Low Inventory':
+                    lowInventory();
+                    break;
+                case 'Add to Inventory':
+                    restockInventory();
+                    break;
+                case 'Add New Product':
+                    newProduct();
+                    break;
+                case 'Exit':
+                    connection.end();
+                    break;
+            }
+        });
+}
+
+function viewInventory() {
+    connection.query(queryAll, function(error, res) {
+        if (error) throw error;
+        console.table(res);
+        managerMenu();
+    });
+}
+
+function lowInventory() {
+    connection.query(`SELECT * FROM products WHERE stock_quantity < 100`, function(error, res) {
+        if (error) throw error;
+        console.table(res);
+        managerMenu();
+    });
+}
+
+function restockInventory() {
+
+}
