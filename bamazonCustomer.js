b59f7c49cec4fc7c5615def6ac5d58668856f8be
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2018-11-02 12:50:33 -0400

    WIP almost done with customer... after breaking a few things

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,11 @@
+const colors = require('colors');
 const inquirer = require('inquirer');
 const mysql = require('mysql');
 const cTable = require('console.table');
 
 const queryAll = "SELECT * FROM products";
+const queryWhere = "WHERE ?";
+let idArray = [];
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -25,9 +28,49 @@ function displayInventory() {
         //console.log(res);
         //connection.end();
         console.table(res);
+        for (let k = 0; k < res.length; k++) {
+            idArray.push(res[k].item_id);
+        }
+
+        queryUsers();
+        //console.log(idArray);
         /*for (let k = 0; k < res.length; k++) {
             console.log(`Item ID: ${res[k].item_id} || Product Name: ${res[k].product_name} || Department: ${res[k].department_name} || Price: ${res[k].price} || Quantity: ${res[k].stock_quantity}`);
         }*/
-        connection.end();
+        //connection.end();
     });
+}
+
+function queryUsers() {
+    inquirer
+        .prompt([
+            {
+                name: 'id',
+                type: 'list',
+                message: 'What is the ID number of the product you would like to purchase?'.cyan,
+                choices: idArray.toString().split(',')
+            },
+            {
+                name: 'quantity_request',
+                type: 'input',
+                message: 'How many would you like to purchase?',
+                validate: function(value) {
+                    return isNaN(value) === false;
+                }
+            }
+        ])
+        .then(function(answers) {
+            //console.log(answers.id, answers.quantity_request);
+            connection.query(`${queryAll} ${queryWhere}`, {item_id: answers.id}, function(err, response) {
+                console.log(`${response[0].item_id}, ${response[0].product_name}, ${response[0].stock_quantity}`);
+                if (response[0].stock_quantity >= answers.quantity_request) {
+                    console.log('you can buy this!'.rainbow);
+                    connection.end();
+                } else {
+                    console.log('Insufficient Quantity! Please make another selection.'.bold.red);
+                    queryUsers();
+                }
+            });
+            //connection.end();
+        });
 }
